c9a3f5c51dfcf3dbcce7499a4fa8fcc7372e3eb8
jmkim0726 <jm0726.kim@gmail.com>
2017-01-31 21:14:33 +0900

    init ver0.2

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
+// index.js
+
 var express = require("express");
 var mongoose = require("mongoose");
+var bodyParser = require("body-parser"); 
 var app = express();
 
-// DB setting
-mongoose.connect(process.env.MONGO_DB); //1
-var db = mongoose.connection; // 2
-
-// 3
+// DB setting...
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
 db.once("open", function(){
     console.log("DB connected");
     });
-
-// 4
 db.on("error", function(err){
     console.log("DB ERROR : ", err);
     });
@@ -19,6 +18,40 @@ db.on("error", function(err){
 // Other settings
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+// DB schema
+var contactSchema = mongoose.Schema({
+    name:{type:String, required:true, unique:true},
+    email:{type:String},
+    phone:{type:String}
+    });
+var Contact = mongoose.model("contact", contactSchema);
+
+// Routes
+// Home
+app.get("/", function(req, res) {
+    res.redirect("/contacts");
+    });
+// Contacts - Index
+app.get("/contacts", function(req, res) {
+    Contact.find({}, function(err, contacts){
+        if(err) return res.json(err);
+	res.render("contacts/index", {contacts:contacts});
+	});
+    });
+// Contacts - New
+app.post("/contacts/new", function(req, res){
+    res.render("contacts/new");
+    });
+// Contacts - Create
+app.post("/contacts", function(req, res){
+    Contact.create(req.body, function(err, contact){
+        if(err) return res.join(err);
+	res.redirect("/contacts");
+	});
+    });
 
 // Port setting
 app.listen(3000, function(){
