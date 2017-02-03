d613e6862ca4e773f74c9451e18a813d41f2a260
jmkim0726 <jm0726.kim@gmail.com>
2017-02-03 14:03:22 +0900

    init ver0.4

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser"); 
+var methodOverride = require("method-override");
 var app = express();
 
 // DB setting...
@@ -20,6 +21,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 // DB schema
 var contactSchema = mongoose.Schema({
@@ -52,6 +54,34 @@ app.post("/contacts", function(req, res){
 	res.redirect("/contacts");
 	})
     });
+// Contacts - Show
+app.get("/contacts/:id", function(req, res){
+    Contact.findOne({_id:req.params.id}, function(err, contact){
+    if(err) return res.json(err);
+    res.render("contacts/show", {contact:contact});
+    });
+});
+// Contacts - Edit
+app.get("/contacts/:id/edit", function(req, res){
+    Contact.findOne({_id:req.params.id}, function(err, contact){
+    if(err) return res.json(err);
+    res.render("contacts/edit", {contact:contact});
+    });
+});
+// Contacts - Update
+app.put("/contacts/:id", function(req, res){
+    Contact.findOneAndUpdate({_id:req.params.id}, req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect("/contacts/"+req.params.id);
+    });
+});
+// Contacts - Destory
+app.delete("/contacts/:id", function(req, res){
+    Contact.remove({_id:req.params.id}, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect("/contacts");
+    });
+});
 
 // Port setting
 app.listen(3000, function(){
